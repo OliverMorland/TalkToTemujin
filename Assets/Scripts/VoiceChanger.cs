144ac0417decfc9ff4479904af37072590b08be2
Oliver Morland <oliversimonedward@hotmail.co.uk>
2024-08-15 16:36:57 -0400

    Added a Brutus portrait

---
+++ Assets/Scripts/VoiceChanger.cs
@@ -0,0 +1,35 @@
+using Meta.WitAi.TTS.Integrations;
+using Meta.WitAi.TTS.Utilities;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor.Presets;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VoiceChanger : MonoBehaviour
+{
+    public TMP_Text voiceName;
+    public Button voiceToggler;
+    public TTSSpeaker ttsSpeaker;
+    public TTSWit ttsWit;
+
+    int counter = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void onVoiceToggled()
+    {
+        
+    }
+}
